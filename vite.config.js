4eb56ff3581fbf8e0cad7eb2300522fc327059ed
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-04-30 00:11:49 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/gitub-resume/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  base: process.env.NODE_ENV === 'production' ? '/github-resume/' : '/',
+
 })
